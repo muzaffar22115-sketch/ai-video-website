410392197da8c82a0a60e8dce7b4f80cfb466a51
muzaffar22115-sketch <muzaffar22115@gmail.com>
2026-09-10 21:06:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
 app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/generate-video", (req, res) => {
 
@@ -8,4 +19,10 @@ app.post("/generate-video", (req, res) => {
     message: "Your AI video request received: " + prompt
   });
 
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("running");
 });
